21bee174cb4c9c744a1836ae20d4cc175acc1755
yq4450 <395380118@qq.com>
2018-12-03 22:28:20 +0800

    小说阅读器

---
+++ static/js/7.1be4107ea9c960f581ff.js
@@ -0,0 +1,2 @@
+webpackJsonp([7],{BlL7:function(e,t,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a={render:function(){var e=this.$createElement;return(this._self._c||e)("div")},staticRenderFns:[]},n=r("C7Lr")({data:function(){return this.$router.replace({name:"chapter",params:{id:this.$route.params.id,page:this.$route.params.page||0,top:0}}),{}}},a,!1,null,null,null);t.default=n.exports}});
+//# sourceMappingURL=7.1be4107ea9c960f581ff.js.map
